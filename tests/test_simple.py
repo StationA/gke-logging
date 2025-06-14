6453b1acf106a4ec5aa77f3be616572e5e206968
Madhulika Chambers <madhulika@stationa.com>
2025-06-13 17:06:22 -0700

    Add a real test

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,2 +1,32 @@
+import logging
+
+from gke_logging.pylogging import GKELoggingFormatter
+
 def test_simple():
-    assert True
+    test_formatter = GKELoggingFormatter()
+
+    # Instantiate a LogRecord using the test fields below
+    name = "my_logger"
+    level = logging.INFO
+    pathname = __file__
+    lineno = 10
+    msg = "This is a test log message"
+    args = ()
+    exc_info = None
+    func = "my_function"
+    sinfo = None
+
+    record = logging.LogRecord(
+        name,
+        level,
+        pathname,
+        lineno,
+        msg,
+        args,
+        exc_info,
+        func,
+        sinfo
+    )
+
+    formatted = test_formatter.format(record)
+    assert formatted
